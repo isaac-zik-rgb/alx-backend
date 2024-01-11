d68e97f98196d73a896ef26950ee15b87a1a0a78
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-11 22:27:13 +0100

    Refactor code to improve performance and readability

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Instatiating babel in Flask app"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel, gettext
+from flask import request
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get_locale
+    """
+    requested_locale = request.args.get('locale')
+    if requested_locale:
+        return requested_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """index
+    """
+    return render_template('4-index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="5000", debug=True)
+
+
+
