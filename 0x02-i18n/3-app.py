62e63871f393dfc587a2d12d68c7d4d688fc6a27
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-12 12:11:02 +0100

    Remove unnecessary blank line in 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -27,7 +27,6 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """The home/index page
