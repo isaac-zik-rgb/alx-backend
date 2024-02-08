1bb85529376443ea72a74b34b84d71d78a074ea3
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-02-08 13:21:40 +0100

    Refactor Redis operations to use async/await

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -23,7 +23,6 @@ async function displaySchoolValue(schoolName) {
 }
 
 
-
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
 displaySchoolValue('HolbertonSanFrancisco');
