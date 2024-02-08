4c6415f14af23d413b1f18341479010ac8e81f12
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-02-08 13:21:22 +0100

    Fix bug in login functionality***
    ***Update error handling in API endpoint***
    
    ***Refactor code for improved performance***
    
    ***Add new feature for user authentication***
    
    ***Update UI layout for better user experience

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,32 @@
+import redis from 'redis'
+const { promisify } = require('util');
+
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+    console.log("Redis client connected to the server");
+});
+
+client.on('error', (err) => {
+    console.error(`Redis client not connected to the server: ${err}`);
+});
+
+async function setNewSchool(schoolName, value) {
+    console.log(await setAsync(schoolName, value, redis.print));
+}
+
+async function displaySchoolValue(schoolName) {
+    console.log(await getAsync(schoolName));
+}
+
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+
+export { client, displaySchoolValue, setNewSchool };
